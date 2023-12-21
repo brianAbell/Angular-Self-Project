d307943b9b0c55810ebfd881cdf487deb119eed2
Brian Bell <brian.bell@kinsaleins.com>
2023-12-21 13:41:19 -0500

    task service injection

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-task-list',
@@ -7,6 +9,16 @@ import { Component } from '@angular/core';
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.css'
 })
-export class TaskListComponent {
 
+export class TaskListComponent implements OnInit {
+  // creates a referenceable object - "injected TaskService into the constructor"
+  constructor(private taskService: TaskService) {}
+  
+  // array of tasks
+  tasks: Task[] = [];
+
+  // initialize the component
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
 }
